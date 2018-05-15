e0076013da5e6c8a93a1337bbb739d8f924ca5bf
Victoria <nrdgrrrl@gmail.com>
2018-05-15 22:47:50 +0000

    added twitter bootstrap, beginnings of a menu

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,23 @@ Encore
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
+    .addEntry('js/app', './assets/js/app.js')
     // .addStyleEntry('css/app', './assets/css/app.scss')
 
     // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    /*.enableSassLoader(function(sassOptions) {}, {
+         resolveUrlLoader: false
+     })*/
+     .enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
